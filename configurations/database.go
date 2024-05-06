8aadd1718ce8eb7bd4d624f8c2ec7ebc9949b31a
bagasseptyono <muhammadbagasmbs@gmail.com>
2024-05-06 18:50:29 +0700

    tes

--- configurations/database.go
+++ configurations/database.go
@@ -21,7 +21,7 @@ func DBConnection() (*sql.DB, error) {
 	dbName := os.Getenv("DB_NAME")
 	params := os.Getenv("DB_PARAMS")
 	// Buat string koneksi database
-	dbConnectionString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?%s", dbUser, dbPassword, dbHost, dbPort, dbName, params)
+	dbConnectionString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?%s", dbUser, dbPassword, dbHost, dbPort, dbName, params)
 
 	// Buat koneksi ke database
 	db, err := sql.Open("postgres", dbConnectionString)
